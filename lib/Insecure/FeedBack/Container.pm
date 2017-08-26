b95b2ffb095fdf2db56fb583c11dba95774b1fa5
Colin Newell <colin.newell@gmail.com>
2017-08-26 21:49:46 +0100

    Allow config to be set outside code

--- lib/Insecure/FeedBack/Container.pm
+++ lib/Insecure/FeedBack/Container.pm
@@ -1,3 +1,5 @@
+package Insecure::FeedBack::Container;
+
 use strict;
 use warnings;
 our @ISA = qw(Exporter);
@@ -25,19 +27,27 @@ my $services = container 'Services' => as {
 
 Bread::Board::set_root_container($services);
 
-my $config = container 'Config' => as {
-    service key => 'test';
-    service 'passwords.min' => 10;
-    service 'passwords.max' => 20;
-};
+my $config = container 'Config' => as {};
+
+my %defaults = (
+    key => 'test',
+    'passwords.max' => 20,
+    'passwords.min' => 10,
+);
+
+# Look for config in env or use the defaults above.
+for my $key (qw/key passwords.min passwords.max/)
+{
+    my $ekey = 'INSECURE_FEEDBACK_' . uc ($key =~ s/\./_/gr);
+    $config->add_service(service $key => $ENV{$ekey} // $defaults{$key});
+}
 
 no Bread::Board; # removes keywords
 
 sub service
 {
-    $DB::single = 1;
     return $services->resolve(service => shift, @_);
 }
  
  
-
+1;
